1a83306ef5a014c7ff2b799bb76d36d3bf0add0b
Mohamed Elfeki <m.elfeki11@gmail.com>
2024-02-17 05:49:59 -0800

    try app

--- WebPage/script.js
+++ WebPage/script.js
@@ -1,79 +1,61 @@
-// // function scrambleText() {
-// //     const inputText = document.getElementById('userInput').value;
-// //     fetch('https://text-mixer.com/api/scramble', {
-// //         method: 'POST',
-// //         headers: {
-// //             'Content-Type': 'application/json'
-// //         },
-// //         body: JSON.stringify({ text: inputText })
-// //     })
-// //     .then(response => response.json())
-// //     .then(data => {
-// //         document.getElementById('scrambledOutput').innerText = data.scrambled;
-// //     })
-// //     .catch(error => console.error('Error:', error));
-// // }
+const url = 'https://api.text-mixer.com/'; // Use HTTPS and the domain name
 
+function detectDials() {
+    const message = document.getElementById('message').value;
+    fetch(url + 'detect_dials', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: message })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const dialsDisplay = document.getElementById('dials');
+        dialsDisplay.innerHTML = '';
+        for (const key in data) {
+            const dial = document.createElement('div');
+            dial.innerText = `${key}: ${data[key]}`;
+            dialsDisplay.appendChild(dial);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
 
-// // const url = 'http://192.168.0.22:5000/';
-// const url = 'http://4.242.50.139:8000/';
-// // const url = 'https://server.text-mixer.com/:8000/';
-// function detectDials() {
-//     const message = document.getElementById('message').value;
-//     fetch(url + 'detect_dials', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ message: message })
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//         const dialsDisplay = document.getElementById('dials');
-//         dialsDisplay.innerHTML = '';
-//         for (const key in data) {
-//             const dial = document.createElement('div');
-//             dial.innerText = `${key}: ${data[key]}`;
-//             dialsDisplay.appendChild(dial);
-//         }
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
-// }
+function rephraseDraft() {
+    const message = document.getElementById('message').value;
+    const instruction = 'funnier and more positive';
+    const responseDisplay = document.getElementById('rephrase');
+    responseDisplay.innerText = '';
+    fetch(url + 'rephrase_draft', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: message, instruction: instruction, last_rephrasing: '' })
+    })
+    .then(response => streamResponse(response, responseDisplay))
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
 
-// function rephraseDraft() {
-//     const message = document.getElementById('message').value;
-//     const responseDisplay = document.getElementById('rephrase');
-//     const instruction = 'funnier and more positive';
-//     responseDisplay.innerText = '';
-//     fetch(url + 'rephrase_draft', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ message: message, instruction: instruction, last_rephrasing: '' })
-//     })
-//     .then(response => streamResponse(response, responseDisplay))
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
-// }
-
-// function streamResponse(response, displayElement) {
-//     const reader = response.body.getReader();
-//     function read() {
-//         reader.read().then(({done, value}) => {
-//             if (done) {
-//                 console.log('Stream finished.');
-//                 return;
-//             }
-//             const chunk = new TextDecoder("utf-8").decode(value);
-//             displayElement.innerText = displayElement.innerText + ' ' + chunk;
-//             console.log(chunk);
-//             read();
-//         });
-//     }
-//     read();
-// }
+function streamResponse(response, displayElement) {
+    const reader = response.body.getReader();
+    function read() {
+        reader.read().then(({done, value}) => {
+            if (done) {
+                console.log('Stream finished.');
+                return;
+            }
+            const chunk = new TextDecoder("utf-8").decode(value);
+            displayElement.innerText += ' ' + chunk;
+            console.log(chunk);
+            read();
+        });
+    }
+    read();
+}
